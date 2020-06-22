96129f93e851195a3b307f9b6b50d99d12110264
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-21 22:31:34 -0700

    fixed object error

--- back/DAO.js
+++ back/DAO.js
@@ -4,29 +4,13 @@ const credentials = require('./key.json')
 class DAO {
   constructor() {
     this.uri = `mongodb+srv://${credentials.username}:${credentials.password}@${credentials.cluster}/test?retryWrites=true&w=majority`
-    this.client = new MongoClient(this.uri, { useUnifiedTopology: true } )
-  }
-
-  async listDatabases() {
-    try {
-      await this.client.connect()
-      const databasesList = await this.client.db().admin().listDatabases()
-      console.log("Databases:")
-      databasesList.databases.forEach(db => console.log(` - ${db.name}`))
-    }
-    catch (e) {
-      console.error(e)
-    }
-    finally {
-      await this.client.close()
-    }
+    this.client = new MongoClient(this.uri, { useUnifiedTopology: true })
   }
 
   async createDocument(databaseName, collectionName, documentInfo) {
     try {
       await this.client.connect()
       const result = await this.client.db(databaseName).collection(collectionName).insertOne(documentInfo)
-      console.log(`New document created: ${result}`)
     }
     catch (e) {
       console.error(e)
--- back/server.js
+++ back/server.js
@@ -1,34 +1,38 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 const DAO = require('./DAO')
 
 const app = express()
-const dao = new DAO()
+const saltRounds = 12
 const PORT = 5000
-
-dao.listDatabases()
 
 app.use(express.static(path.join(__dirname, '../front/build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/signupRequest', async (req, res) => {
+  const dao = new DAO()
+
   const email = req.body.email
   const password = req.body.password
 
-  const databaseName = 'users'
-  const collectionName = 'accounts'
-  const documentData = {'email' : email, 'password' : password}
+  bcrypt.hash(password, saltRounds, async (err, hash) => {
+    const databaseName = 'users'
+    const collectionName = 'accounts'
 
-  try {
-    await dao.createDocument(databaseName, collectionName, documentData)
-    res.send({ message: 'success' })
-  }
-  catch (error) {
-    console.error(error)
-    res.send({ message: error.toString() })
-  }
+    const documentData = { 'email' : email, 'password' : hash }
+
+    try {
+      await dao.createDocument(databaseName, collectionName, documentData)
+      res.send({ message: 'success' })
+    }
+    catch (error) {
+      console.error(error)
+      res.send({ message: error.toString() })
+    }
+  })
 })
 
 app.get('/*', (req, res) => {
